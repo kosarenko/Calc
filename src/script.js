b03ff0eb75caca64e1bcdb1eb4c208a85f8c8e25
kosarenko <kosarenko90@gmail.com>
2022-05-19 20:57:18 +0300

    add js file

--- src/script.js
+++ src/script.js
@@ -1,1 +1,15 @@
 console.log('script');
+
+    const displayResultItem = document.getElementById("displayResult");
+
+    function press(value) {
+    displayResultItem.value += value;
+}
+
+    function calculate() {
+    displayResultItem.value = eval(displayResultItem.value);
+}
+
+    function clearDisplay() {
+    displayResultItem.value = "";
+}
