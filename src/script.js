5c742cc1d553edd10ced7c4750b98abe7c805a3f
kosarenko <kosarenko90@gmail.com>
2022-06-03 17:36:10 +0300

    edited html elements

--- src/script.js
+++ src/script.js
@@ -1,14 +1,24 @@
+const displayResultItem = document.getElementById("displayResult");
 
-    const displayResultItem = document.getElementById("displayResult");
+function press(value) {
 
-    function press(value) {
-    displayResultItem.value += value;
+    if (displayResultItem.value == "00") {
+        displayResultItem.value = 0;
+    }else {
+        displayResultItem.value += value;
+    }
 }
 
-    function calculate() {
+function calculate() {
     displayResultItem.value = eval(displayResultItem.value);
 }
 
-    function clearDisplay() {
+function clearDisplay() {
     displayResultItem.value = "";
 }
+
+function signPlusMinus() {
+    displayResultItem.value = 0 - displayResultItem.value
+}
+
+//<div jsname="YovRWb" class="XRsWPe MEdqYd" jsaction="A2jXUd" role="button" tabindex="0" aria-label="умножение" data-ved="2ahUKEwiLyJ-0goz4AhVxxosKHYmcCykQz6kIegQIAhAq">×</div>
